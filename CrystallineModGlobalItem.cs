187ab0093a5a4222254bfc1e9c665ed298860e9e
AndrewIDFK <47414739+AndrewIDFK@users.noreply.github.com>
2020-07-28 09:33:21 +0300

    Optimizations and deleted CloneInstances

--- CrystallineModGlobalItem.cs
+++ CrystallineModGlobalItem.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ModLoader;
-
-
 namespace CrystallineMod
 {
     public class CrystallineModGlobalItem : GlobalItem
     {
+		public override bool InstancePerEntity
+        {
+            get
+            {
+                return true;
+            }
+
+        }
         int OverclockBuff;
         public override bool Shoot(Item item, Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
             if (item.ranged)
             {
-
-                if (player.GetModPlayer<CrystallineModPlayer>().OverclockBuff == true)
+                if (OverclockBuff == 0)
                 {
-                    if (OverclockBuff == 0)
-                    {
+					if (player.GetModPlayer<CrystallineModPlayer>().OverclockBuff == true)
+					{
                         item.useTime -= 1;
                         item.useAnimation -= 1;
                         OverclockBuff = 1;
                     }
-
                 }
-
-                else if(OverclockBuff == 1)
+                else 
                 {
                     OverclockBuff = 0;
                     item.useTime = item.useTime + 1;
@@ -33,44 +33,22 @@ namespace CrystallineMod
             }
             return true;
         }
-        public override bool InstancePerEntity
-        {
-            get
-            {
-                return true;
-            }
-
-        }
-
-
-        public override bool CloneNewInstances
-        {
-            get
-            {
-                return true;
-            }
-
-        }
-
+		
         int OverclockBuff2;
         public override void OnConsumeAmmo(Item item, Player player)
         {
             if (item.ranged)
             {
-                if (player.GetModPlayer<CrystallineModPlayer>().OverclockBuff == true)
+                if (OverclockBuff2 == 0)
                 {
-
-                    if (OverclockBuff2 == 0)
+                    if (player.GetModPlayer<CrystallineModPlayer>().OverclockBuff == true)
                     {
                         item.useTime -= 1;
                         item.useAnimation -= 1;
                         OverclockBuff2 = 1;
                     }
-
-
                 }    
-                
-                else if(OverclockBuff2 == 1)
+                else 
                 {
                     OverclockBuff2 = 0;
                     item.useTime = item.useTime + 1;
@@ -78,6 +56,5 @@ namespace CrystallineMod
                 }
             }
         }
-
     }
 }
